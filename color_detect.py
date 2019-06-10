6a07931a32455c8e31458bf728f23f5c5577ed56
Wojciech Wetula <wetula.wojciech@gmail.com>
2019-06-10 07:49:13 +0200

    Zwracamy maskę

--- color_detect.py
+++ color_detect.py
@@ -15,9 +15,9 @@ def detect_red(image):
 
     mask = cv2.bitwise_or(mask1, mask2)
 
-    result = cv2.bitwise_and(image, image, mask=mask)
+    #result = cv2.bitwise_and(image, image, mask=mask)
 
-    return result
+    return mask
 
 def detect_blue(image):
     lower_blue = np.array([100, 150, 0])
@@ -25,21 +25,21 @@ def detect_blue(image):
 
     mask = cv2.inRange(image, lower_blue, upper_blue)
 
-    result = cv2.bitwise_and(image, image, mask=mask)
+    #result = cv2.bitwise_and(image, image, mask=mask)
 
-    return result
+    return mask
 
 
 def detect_red_and_blue(image):
-    lower_blue = np.array([100, 150, 0])
-    upper_blue = np.array([140, 255, 255])
+    lower_blue = np.array([110, 150, 50])
+    upper_blue = np.array([130, 255, 255])
     mask0 = cv2.inRange(image, lower_blue, upper_blue)
 
-    lower_red = np.array([0, 50, 50])
+    lower_red = np.array([0, 70, 50])
     upper_red = np.array([5, 255, 255])
     mask1 = cv2.inRange(image, lower_red, upper_red)
 
-    lower_red = np.array([175, 50, 50])
+    lower_red = np.array([175, 70, 50])
     upper_red = np.array([180, 255, 255])
     mask2 = cv2.inRange(image, lower_red, upper_red)
 
@@ -47,6 +47,6 @@ def detect_red_and_blue(image):
     mask = cv2.bitwise_or(mask1, mask2)
 
     result = cv2.bitwise_and(image, image, mask=mask)
-    return result
+    return mask
 
 
